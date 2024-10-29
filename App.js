85e14676fe5335d648c2f5ad28278c8c0b9fdae1
22044242-MohammadHarris <22044242@myrp.edu.sg>
2024-10-29 15:35:28 +0800

    Lesson 3 - Exercise 2

---
+++ App.js
@@ -0,0 +1,113 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Question from './components/Question';
+import { useState } from 'react';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+export default function App() {
+
+  const [answer, setAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [isReset, setIsReset] = useState(false);
+
+
+  const options = [
+    {
+      label: "cat",
+      value: "cat",
+      image: require('./img/cat.jpg')
+    },
+    {
+      label: "deer",
+      value: "deer",
+      image: require('./img/deer.jpg')
+    },
+    {
+      label: "fox",
+      value: "fox",
+      image: require('./img/fox.jpg')
+    },
+    {
+      label: "giraffe",
+      value: "giraffe",
+      image: require('./img/giraffe.jpg')
+    },
+    {
+      label: "panda",
+      value: "panda",
+      image: require('./img/panda.jpg')
+    },
+    {
+      label: "penguin",
+      value: "penguin",
+      image: require('./img/penguin.jpg')
+    },
+    {
+      label: "rabbit",
+      value: "rabbit",
+      image: require('./img/rabbit.jpg')
+    },
+    {
+      label: "snake",
+      value: "snake",
+      image: require('./img/snake.jpg')
+    },
+    {
+      label: "tiger",
+      value: "tiger",
+      image: require('./img/tiger.jpg')
+    }
+  ];
+
+
+  const handleScore = () => {
+    setScore(answer?.length)
+    const medianScore = options.length/2;
+
+    if(score < medianScore){
+      Alert.alert("Your score is below average, try again!!", `Score: ${answer?.length} / ${options.length}`)
+    }
+    if(score > medianScore){
+      Alert.alert("Your score is above average, well done!!",`Score: ${answer?.length} / ${options.length}`)
+    }
+    if(score == medianScore){
+      Alert.alert("Your score is average, good job!!",`Score: ${answer?.length} / ${options.length}`)
+    }
+  }
+
+  
+  return (
+    <View style={styles.container}>
+      <View style={{flexDirection:'row', alignItems:'center',marginVertical:5}}>
+        {/* <FontAwesome5 name="cat" size={24} color="black" /> */}
+        <MaterialIcons name="quiz" size={24} color="black" />
+        <Text style={{marginLeft:10}}>Animals Quiz App</Text>
+      </View>
+     
+      <ScrollView>
+      
+      {options?.map((option, index)=>(
+        <Question key={index} resetObj={{isReset,setIsReset}} answerObj={{answer,setAnswer}} options={options} mainAnswer={option.label} image={option.image} questionSequence={index++}/>
+      ))}
+
+      <Button title='SUBMIT ANSWERS' onPress={handleScore}/>
+      <View style={{marginVertical:7}}/>
+
+      <Button onPress={()=>{
+        setIsReset(true)
+      }} title='reset'/>
+
+      <Text>{JSON.stringify(answer)}</Text>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop:60,
+    margin:10
+  },
+});
---
+++ components/Question.js
@@ -0,0 +1,65 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import RNPickerSelect from "react-native-picker-select";
+
+const Question = ({ options, image,resetObj, answerObj, mainAnswer, 
+    questionSequence
+ }) => {
+  const { answer, setAnswer } = answerObj;
+  const {isReset, setIsReset} = resetObj
+  const [selected, setSelected] = useState("");
+
+  // set Updated answer based on selection
+  const handleAnswer = (optionSelected) => {
+    setSelected(optionSelected)
+    if (optionSelected == mainAnswer) {
+    //   console.log("correct");
+      
+      if (!answer.includes(mainAnswer+questionSequence)) {
+        setAnswer((prev) => [...prev, optionSelected+questionSequence]);
+      }
+
+    } else {
+      setAnswer((prev) => prev.filter((ans) => ans !== mainAnswer+questionSequence));
+      
+    //   console.log(answer);
+    //   console.log("wrong");
+    }
+  };
+
+  useEffect(()=>{
+    if(isReset==true){
+        setSelected("")
+        setAnswer([])
+        setIsReset(false)
+    }
+  },[isReset])
+
+
+  return (
+    <View style={{ marginVertical: 10 }}>
+      <View>
+        <Text style={{ color: "gray" }}>What animal is this?</Text>
+
+        {/* Select options */}
+
+        <RNPickerSelect
+          onValueChange={(option) => {
+            handleAnswer(option);
+          }}
+          value={selected}
+          items={options}
+        />
+
+        {/* image */}
+        <Image source={image} style={{ width: "100%", height: 400 }} />
+      </View>
+
+     
+    </View>
+  );
+};
+
+export default Question;
+
+const styles = StyleSheet.create({});
